1719fa409ee30e1c7a4143fb3f3eafd4b21ea56b
Mark <niujl123@sina.com>
2020-03-21 17:15:02 +0800

    pandapi pins update

--- pandapi_marlin2.0_test_version/Configuration.h
+++ pandapi_marlin2.0_test_version/Configuration.h
@@ -1372,7 +1372,7 @@
 
 // Homing speeds (mm/m)
 #define HOMING_FEEDRATE_XY (50*60/2)
-#define HOMING_FEEDRATE_Z  (4*60)
+#define HOMING_FEEDRATE_Z  (10*60)
 
 // Validate that endstops are triggered on homing moves
 #define VALIDATE_HOMING_ENDSTOPS
--- pandapi_marlin2.0_test_version/Configuration_adv.h
+++ pandapi_marlin2.0_test_version/Configuration_adv.h
@@ -2070,7 +2070,7 @@
 
   #if AXIS_IS_TMC(X)
     #define X_CURRENT       800        // (mA) RMS current. Multiply by 1.414 for peak current.
-    #define X_CURRENT_HOME  1  // (mA) RMS current for sensorless homing
+    #define X_CURRENT_HOME  100  // (mA) RMS current for sensorless homing
     #define X_MICROSTEPS     16    // 0..256
     #define X_RSENSE          0.11
     #define X_CHAIN_POS      -1    // <=0 : Not chained. 1 : MCU MOSI connected. 2 : Next in chain, ...
@@ -2086,7 +2086,7 @@
 
   #if AXIS_IS_TMC(Y)
     #define Y_CURRENT       800
-    #define Y_CURRENT_HOME  3
+    #define Y_CURRENT_HOME  200
     #define Y_MICROSTEPS     16
     #define Y_RSENSE          0.11
     #define Y_CHAIN_POS      -1
