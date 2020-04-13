2ab9e405cc0bbdafab2de98f983abe4c07687247
Mark <niujl123@sina.com>
2020-04-13 10:10:39 +0800

    set default value of BLtouch probe offset https://www.thingiverse.com/thing:3584158

--- Marlin2.x/pandapi/Configuration.h
+++ Marlin2.x/pandapi/Configuration.h
@@ -956,7 +956,7 @@
  *
  * Specify a Probe position as { X, Y, Z }
  */
-#define NOZZLE_TO_PROBE_OFFSET { 10, 10, 0 }
+#define NOZZLE_TO_PROBE_OFFSET { -40, -30, -2.5 } //for mount BLtouch.https://www.thingiverse.com/thing:3584158 
 
 // Most probes should stay away from the edges of the bed, but
 // with NOZZLE_AS_PROBE this can be negative for a wider probing area.
