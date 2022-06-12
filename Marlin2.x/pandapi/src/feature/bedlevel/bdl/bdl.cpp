97fe6d6b818cb59af6bd63b7f52b6129bb6b1d14
Mark <niujl123@sina.com>
2022-06-12 11:17:33 +0800

    Update bdl.cpp

--- Marlin2.x/pandapi/src/feature/bedlevel/bdl/bdl.cpp
+++ Marlin2.x/pandapi/src/feature/bedlevel/bdl/bdl.cpp
@@ -60,15 +60,14 @@ will do adjusting while the Z height <=0.4mm , disable it by M102 S0.
 I2C_SegmentBED BD_I2C_SENSOR;
 
 
-void Bed_Distance_sensor_level::echo_name() { SERIAL_ECHOPGM("unified bed 
-leveling"); }
+void Bed_Distance_sensor_level::echo_name() { }
 
 void Bed_Distance_sensor_level::init(unsigned char _sda,unsigned char _scl,
 int delay_s) {
 
   int ret=BD_I2C_SENSOR.i2c_init(_sda,_scl,0x3C,delay_s);
   if(ret!=1)
-    SERIAL_ECHOLNPGM("BD_I2C_SENSOR Init Fail return code:",ret);
+    printf("BD_I2C_SENSOR Init Fail return code:%d\n",ret);
   BDsensor_config=0;
 }
 
@@ -93,15 +92,13 @@ void Bed_Distance_sensor_level::BD_sensor_process(void){
       z_sensor=(tmp&0x3ff)/100.0;
       if(cur_z<0)
         BDsensor_config=0;
-      //float abs_z=current_position.z>cur_z?(current_position.z-cur_z):(cur_z
--current_position.z);
+      //float abs_z=current_position.z>cur_z?(current_position.z-cur_z):(cur_z-current_position.z);
       if(cur_z<(BDsensor_config/10.0)&&(BDsensor_config>0)
-        &&(old_cur_z==cur_z)&&(old_buf_z==current_position.z)&&(z_sensor<(
-MAX_BD_HEIGHT))){
+        &&(old_cur_z==cur_z)&&(old_buf_z==current_position.z)&&(z_sensor<(MAX_BD_HEIGHT))){
           
           babystep.set_mm(Z_AXIS,(cur_z-z_sensor));
 #if DEBUG_OUT_BD          
-          SERIAL_ECHOLNPGM("BD:",z_sensor,", Z:",cur_z,"|",current_position.z);
+          printf("BD:%.2f,Z:%.2f|%.2f\n",z_sensor,cur_z,current_position.z);
 #endif          
       }
       else{
@@ -119,9 +116,9 @@ MAX_BD_HEIGHT))){
     else
       stepper.set_directions();
 #if DEBUG_OUT_BD      
-    SERIAL_ECHOLNPGM("BD:",tmp&0x3ff,", Z:",cur_z,"|",current_position.z);
+    printf("BD:%d,Z:%.2f|%.2f",tmp&0x3ff,cur_z,current_position.z);
     if(BD_I2C_SENSOR.BD_Check_OddEven(tmp)==0)
-      SERIAL_ECHOLNPGM("errorCRC");        
+      printf("errorCRC\n");        
 #endif    
     if((tmp&0x3ff)>1020){
       BD_I2C_SENSOR.BD_i2c_stop();
@@ -134,8 +131,7 @@ MAX_BD_HEIGHT))){
          
       for(int i=0;i<MAX_BD_HEIGHT*10;i++){
         tmp=BD_I2C_SENSOR.BD_i2c_read();    
-        SERIAL_ECHOLNPGM("Calibrate data:",i,",",tmp&0x3ff,", check:",
-BD_I2C_SENSOR.BD_Check_OddEven(tmp));
+        printf("Calibrate data:%d,%d,check:%d\n",i,tmp&0x3ff,BD_I2C_SENSOR.BD_Check_OddEven(tmp));
         safe_delay(500);
       }
       BDsensor_config=0; 
@@ -159,27 +155,26 @@ BD_I2C_SENSOR.BD_Check_OddEven(tmp));
         z_pose=0;
         safe_delay(1000);
         BD_I2C_SENSOR.BD_i2c_write(CMD_START_CALIBRATE);// begain calibrate //
-        SERIAL_ECHOLNPGM("Begain calibrate");
+        printf("Begain calibrate\n");
         safe_delay(2000);
         BDsensor_config=-7;
       }
       else if(planner.get_axis_position_mm(Z_AXIS)<10.0){        
         if(z_pose>=MAX_BD_HEIGHT){
             BD_I2C_SENSOR.BD_i2c_write(CMD_END_CALIBRATE); // end calibrate  //
-            SERIAL_ECHOLNPGM("End Calibrate data");
+            printf("End Calibrate data\n");
             z_pose=7;
             BDsensor_config=0;
             safe_delay(1000);
         }
         else{
           float tmp_k=0;
-          sprintf_P(tmp_1,  PSTR("G1 Z%d.%d"), (int)z_pose,(int)((int)(z_pose*
-10)%10));
+          sprintf_P(tmp_1,  PSTR("G1 Z%d.%d"), (int)z_pose,(int)((int)(z_pose*10)%10));
           parser.parse(tmp_1);
           gcode.process_parsed_command();
         //  current_position.z
           SERIAL_ECHOPGM(tmp_1);
-          SERIAL_ECHOLNPGM(" ,Z:",current_position.z);
+          printf(" Z:%.2f\n",current_position.z);
           while((tmp_k+0.1)<z_pose){
             tmp_k=planner.get_axis_position_mm(Z_AXIS);
             safe_delay(1);
@@ -187,7 +182,7 @@ BD_I2C_SENSOR.BD_Check_OddEven(tmp));
           safe_delay(800);
           tmp=(z_pose+0.0001)*10;
           BD_I2C_SENSOR.BD_i2c_write(tmp);
-          SERIAL_ECHOLNPGM("w:",tmp,",Zpose:",z_pose);
+          printf("w:%d ,Zpose:%.2f",tmp,z_pose);
           z_pose+=0.1001;
          // queue.enqueue_now_P(PSTR("G90"));
         }
